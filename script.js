10e3f145af403a902e51c2d6c0fa7f0432023548
John2175 <158193559+John2175@users.noreply.github.com>
2024-01-30 12:53:26 -0500

    body pages

---
+++ script.js
@@ -0,0 +1,80 @@
+
+
+function decodeJwtResponse(token) {
+  var base64Url = token.split(".")[1];
+  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  var jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
+
+  return JSON.parse(jsonPayload);
+}
+
+function onSignIn(responsePayload){
+  var name = document.getElementsByClassName("profile_name");
+  var pic = document.getElementsByClassName("profile_pic");
+  for(var i = 0; i < name.length; i++){
+    name[i].innerText=responsePayload.name;   
+    pic[i].src=responsePayload.picture;
+    }
+}
+
+
+function handleCredentialResponse(response) {
+  // decodeJwtResponse() is a custom function defined by you
+  // to decode the credential response.
+  const responsePayload = decodeJwtResponse(response.credential);
+
+  console.log("ID: " + responsePayload.sub);
+  console.log('Full Name: ' + responsePayload.name);
+  console.log('Given Name: ' + responsePayload.given_name);
+  console.log('Family Name: ' + responsePayload.family_name);
+  console.log("Image URL: " + responsePayload.picture);
+  console.log("Email: " + responsePayload.email);
+  onSignIn(responsePayload);
+}
+
+
+
+
+
+
+
+
+function toggleMenu()
+{
+  var menu = document.getElementById("myMenu");
+  var menuicon = document.getElementById("menu-icon");
+  if (menu.style.width === "150px") 
+  {
+  //  menuicon.textContent = "|||"
+    menu.style.width = "0";
+  } 
+  else 
+  {
+  //  menuicon.textContent = "☰"
+    menu.style.width = "150px";
+  }
+}
+
+function onFocusMain()
+{
+  var menu = document.getElementById("myMenu");
+  if (menu.style.width === "150px") 
+  {
+  //  menuicon.textContent = "☰"
+    menu.style.width = "0";
+  }
+}
+
+function navigate(page)
+{
+  window.location.href = "../" + page + ".html"
+}
+
+
